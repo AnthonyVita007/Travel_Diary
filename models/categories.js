a77052df2fd1df4c23a0a807e58131a2315268f2
AnthonyVita007 <vitaanthony26@gmail.com>
2025-07-04 17:43:48 +0200

    inserimento del file categories.js per maggiore ordine

---
+++ models/categories.js
@@ -0,0 +1,54 @@
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export const categories = [
+  {
+    id: 1,
+    name: 'Nature',
+    icon: 'leaf',
+    color: '#4CAF50' // verde
+  },
+  {
+    id: 2,
+    name: 'Safari',
+    icon: 'elephant',
+    color: '#D2B48C' // sabbia
+  },
+  {
+    id: 3,
+    name: 'Off Road',
+    icon: 'car',
+    color: '#8B4513' // marrone
+  },
+  {
+    id: 4,
+    name: 'City Life',
+    icon: 'city',
+    color: '#87CEEB' // celeste
+  },
+  {
+    id: 5,
+    name: 'Beach',
+    icon: 'beach',
+    color: '#1E90FF' // blu intenso
+  },
+  {
+    id: 6,
+    name: 'Mountain',
+    icon: 'hiking',
+    color: '#708090' // grigio ardesia
+  },
+  {
+    id: 7,
+    name: 'Culture',
+    icon: 'book',
+    color: '#9370DB' // viola medio
+  },
+  {
+    id: 8,
+    name: 'Food & Wine',
+    icon: 'food-fork-drink',
+    color: '#FF6347' // rosso pomodoro
+  }
+];
+
+export default categories;
